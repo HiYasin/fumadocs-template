bbcf572b4055f73abda4bc63fc741c0fb02a6d99
hiyasin <c223059@ugrad.iiuc.ac.bd>
2026-06-05 03:37:28 +0600

    chore: update dependencies and add new packages
    - Added new dependencies: @hugeicons/core-free-icons, @hugeicons/react, class-variance-authority, clsx, radix-ui, shadcn, and tw-animate-css.
    - Updated devDependencies: @biomejs/biome.
    - Removed duplicate entry for @biomejs/biome in devDependencies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
+import { Inter, Figtree } from 'next/font/google';
 import type { Metadata } from 'next'
+import { cn } from "@/lib/utils";
+
+const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   subsets: ['latin'],
@@ -18,7 +21,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={cn(inter.className, "font-sans", figtree.variable)} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
