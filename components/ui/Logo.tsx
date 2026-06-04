8fef175b51e1ab8c0f3278b10f74bfbaa7f0b698
hiyasin <c223059@ugrad.iiuc.ac.bd>
2026-06-04 20:37:40 +0600

    feat: add BlogPage and Logo components; update navbar in layout to include Logo and page links

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,9 @@
+export const Logo = () => {
+  return (
+    <div>
+        <h1>
+            Faisal's <span className="text-fd-primary dark:text-blue-300">FumaDocs</span>
+        </h1>
+    </div>
+  )
+}
--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,12 +1,25 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { appName, gitConfig } from './shared';
+import { Logo } from '@/components/ui/Logo';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       // JSX supported
-      title: appName,
+      title: <Logo />,
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
+    links: [
+      {
+        text: "Documentation",
+        url: "/docs",
+        active: "nested-url"
+      },
+      {
+        text: "Blog",
+        url: "/blog",
+        active: "nested-url"
+      },
+    ]
   };
 }
