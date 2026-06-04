afa3367ee1633b38917ad0f72c98d71d2803698f
hiyasin <c223059@ugrad.iiuc.ac.bd>
2026-06-04 21:24:46 +0600

    fix: add metadata export for Vercel deployments and local development to resolve metadata error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
+import type { Metadata } from 'next'
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  metadataBase: process.env.VERCEL_URL 
+    ? new URL(`https://${process.env.VERCEL_URL}`)  // For Vercel deployments
+    : new URL('http://localhost:3000'),              // For local development
+  
+  title: 'Faial Fumadocs',
+  description: 'This site is built with Fumadocs, the headless documentation framework for MDX-based docs sites.',
+}
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
